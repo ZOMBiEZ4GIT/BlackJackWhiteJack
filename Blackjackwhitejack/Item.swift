1118d5a3c274ce2f9a6fffcc92f806527c26cbd5
RolandGeorge <154392091+RolandGeorge@users.noreply.github.com>
2025-11-21 19:51:06 +1100

    Updated specifications document with new requirements and clarifications.

---
+++ Blackjackwhitejack/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Blackjackwhitejack
+//
+//  Created by Roland on 21/11/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
